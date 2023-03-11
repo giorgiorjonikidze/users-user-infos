28cc16bf3b1ef7850f9ff40b985df52ca78fe428
giorgi orjonikidze <gioorjo123@gmail.com>
2023-03-11 15:38:41 +0400

    add hover color to userListing

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -3,13 +3,13 @@ const User = ({ firstname, lastname, image, id, openUserHandler }) => {
         openUserHandler(id)
     }
   return (
-    <div onClick={openUser} className="w-[150px] h-[150px] flex flex-col items-center justify-center border-[2px] border-solid border-[#000000] cursor-pointer">
+    <div onClick={openUser} className="w-[150px] h-[150px] flex flex-col items-center justify-center border-[2px] border-solid border-[#000000] cursor-pointer hover:text-[#06b6d4]">
       <img
         src={image}
         alt="user image"
         className="rounded-full w-[50px] h-[50px] mt-[5px] mb-[20px]"
       />
-      <div className="text-[14px] flex">
+      <div className="text-[14px] flex ">
         <p className="font-bold">{firstname}</p>
         <p className="font-bold ml-[5px]">{lastname}</p>
       </div>
